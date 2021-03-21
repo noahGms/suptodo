187b2322bca9b422570a97f6439cf3de2efc9845
noahGms <noah@keshin.net>
2021-03-21 23:02:42 +0100

    updated friends relation

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
-            'friends' => FriendResource::collection($this->Friends)
+            'friends' => FriendResource::collection($this->Friends()->where('accepted', 1)->get())
         ];
     }
 }
